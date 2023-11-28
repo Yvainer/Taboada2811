b92890fb063dcda0daafdf1ca4f9367603dac63c
kaualira <130668344+kaualira@users.noreply.github.com>
2023-11-28 11:11:20 -0300

    - Comit da taboada meia boca

---
+++ taboada.py
@@ -0,0 +1,19 @@
+# Taboada
+
+# apresentaçaõ
+print('\n\t\t\t -- Taboada --\n')
+
+t = int(input('Informe o número: '))
+
+
+#processamento e Saída
+print(f'{t} * 1 = {t*1}')
+print(f'{t} * 2 = {t*2}')
+print(f'{t} * 3 = {t*3}')
+print(f'{t} * 4 = {t*4}')
+print(f'{t} * 5 = {t*5}')
+print(f'{t} * 6 = {t*6}')
+print(f'{t} * 7 = {t*7}')
+print(f'{t} * 8 = {t*8}')
+print(f'{t} * 9 = {t*9}')
+print(f'{t} * 10 = {t*10}')
